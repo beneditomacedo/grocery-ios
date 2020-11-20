46ddf234d4722abbd57656bf5baa5b34c2faa696
Benedito Alberto Macedo <benedito.macedo@gmail.com>
2020-11-20 20:11:33 -0300

    Initial Commit

---
+++ grocery-ios/grocery_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  grocery_iosApp.swift
+//  grocery-ios
+//
+//  Created by bam on 20/11/20.
+//
+
+import SwiftUI
+
+@main
+struct grocery_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
